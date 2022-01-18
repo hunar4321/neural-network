1644c50696c30ffe62ed12a931945a610c17e7df
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2022-01-18 21:53:57 +0300

    Update solve_xor.m

--- solve_xor.m
+++ solve_xor.m
@@ -3,27 +3,31 @@
 %% @author: Hunar Ahmad Abdulrahman
 
 %% xor data
-X=[0,0; 1,1; 1,0; 0,1];  %inputs
+X1=[0,0; 1,1; 1,0; 0,1];  %inputs
 y=[0; 0; 1; 1];          %outputs
 
 % model settings 
-learn_rate=0.1;
+lr=0.1;
 in=2; nodes=4; out =1;
 % weight initialization
 W1=randn(in,nodes);
 W2=randn(nodes,out);
 %% training loop...
-for i=1:1000;
+for i=1:100;
     % feedforward
-    z1=X*W1;
+    z1=X1*W1;
     X2=sin(z1); % "sin" used as "activation function" for simplicity
     z2=X2*W2;
     yhat=sin(z2);
     % backpropagation
-    delta2 = (y-yhat).*cos(z2); % "cos" is derivative of "sin"
-    W2=W2+(X2'*delta2) *learn_rate;
-    delta1 = delta2*W2'.*cos(z1);
-    W1=W1+(X'*delta1) *learn_rate;
+    e1 = y - yhat
+    delta2 = e1.*cos(z2); % "cos" is derivative of "sin"
+    e2 = (delta2*W2')
+    delta1 = e2.* cos(z1);
+    dW2 = X2'* delta2;
+    dW1 = X1'* delta1;
+    W2=W2+ dW2 *lr;
+    W1=W1+ dW1 *lr;
     % optional line ( squared error for visualization);
     mse(i)=mean((y-yhat).^2);
 end
