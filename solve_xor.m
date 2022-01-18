f3cd798a80ece039ab34e9a582d551e35f708946
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2022-01-18 22:16:28 +0300

    Update solve_xor.m

--- solve_xor.m
+++ solve_xor.m
@@ -34,3 +34,49 @@ end
 %% output & error visualization
 plot(mse); title('MSE');
 [yhat, y] % compare the predictions with the true labels
+
+%%% stochastic version (SGD)
+% clear
+% %% xor data
+% X1=[0,0; 1,1; 1,0; 0,1];  %inputs
+% y=[0; 0; 1; 1];          %outputs
+% % model settings 
+% lr=0.1;
+% in=2; nodes=4; out =1;
+% % weight initialization
+% W1=randn(in,nodes);
+% W2=randn(nodes,out);
+% %% training loop...
+% for i=1:100;
+%     % feedforward
+%     me = 0
+%     for j=1:4;
+%         z1=X1(j,:)*W1;
+%         X2=sin(z1); % "sin" used as "activation function" for simplicity
+%         z2=X2*W2;
+%         yhat=sin(z2);
+%         % backpropagation
+%         e1 = y(j) - yhat;
+%         delta2 = e1.*cos(z2); % "cos" is derivative of "sin"
+%         e2 = (delta2*W2');
+%         delta1 = e2.* cos(z1);
+%         dW2 = X2'* delta2;
+%         dW1 = X1(j,:)'* delta1;
+%         W2=W2+ dW2 *lr;
+%         W1=W1+ dW1 *lr;
+%         
+%         me = me + ( y(j)-yhat ).^2;
+%     end
+%     mse(i)= me;
+% end
+% %% output & error visualization
+% plot(mse); title('MSE');
+% for j=1:4;
+%     z1=X1(j,:)*W1;
+%     X2=sin(z1); % "sin" used as "activation function" for simplicity
+%     z2=X2*W2;
+%     yhat=sin(z2);
+%     [y(j), yhat]
+% end
+
+
