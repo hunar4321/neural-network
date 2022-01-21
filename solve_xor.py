8b673f39732103fea9f50152e69aec97be5360ad
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2022-01-21 11:35:19 +0300

    Update solve_xor.py

--- solve_xor.py
+++ solve_xor.py
@@ -16,75 +16,77 @@ y = [[0],[0],[1],[1]]
 x = np.asarray(x)
 y = np.asarray(y)
 
-hidden = 4
+hidden = 5
 inp = x.shape[1]
 out = y.shape[1]
 Epochs = 1000
-lr = 0.1
+lr = 0.3
 
 Wx = np.random.rand(inp, hidden)
 Wo = np.random.rand(hidden, out)
 
-## online version
+### offline version
 es = []
 for ep in range(Epochs):
-    for i in range(y.shape[0]):
-        z1 = x[i] @ Wx
-        z1 = np.sin(z1)
-        z2 = z1 @ Wo
-        z2 = np.sin(z2)
-        e1 = y[i]-z2        
-        de1 = e1 * deriv(z2)
-        e2 = de1 * Wo
-        de2 = e2.T * deriv(z1)
+    z1 = x @ Wx
+    a1 = act(z1)
+    z2 = a1 @ Wo
+    a2 = act(z2)
+    e1 = y-a2        
+    de1 = e1 * deriv(z2)
+    e2 = de1 * Wo.T
+    de2 = e2 * deriv(z1)
 
-        dWo = np.outer( z1 , de1 )
-        dWx = np.outer( x[i], de2)
-        Wo = Wo + dWo*lr
-        Wx = Wx + dWx*lr
-        
-        es.append(abs(e1))
+    dWo = np.dot( z1.T , de1 )
+    dWx = np.dot( x.T, de2)
+    Wo = Wo + dWo*lr
+    Wx = Wx + dWx*lr
+    
+    es.append(np.sum(np.abs(e1)))
 
 
 plt.figure(1)
 plt.plot(es)
 
-for i in range(y.shape[0]):
-    z0 = x[i] @ Wx
-    z0 = np.sin(z0)
-    z1 = z0 @ Wo
-    z1 = np.sin(z1)
-    
-    print(y[i], z1)
-    
-    
-### offline version
+z0 = x @ Wx
+z0 = act(z0)
+z1 = z0 @ Wo
+z1 = act(z1)
+
+for i in range(len(y)):
+    print(y[i], z1[i])
+
+
+# ## online version
 # es = []
 # for ep in range(Epochs):
-#     z1 = x @ Wx
-#     z1 = np.sin(z1)
-#     z2 = z1 @ Wo
-#     z2 = np.sin(z2)
-#     e1 = y-z2        
-#     de1 = e1 * deriv(z2)
-#     e2 = de1 * Wo.T
-#     de2 = e2 * deriv(z1)
+#     for i in range(y.shape[0]):
+#         z1 = x[i] @ Wx
+#         a1 = act(z1)
+#         z2 = a1 @ Wo
+#         a2 = act(z2)
+#         e1 = y[i]-a2        
+#         de1 = e1 * deriv(z2)
+#         e2 = de1 * Wo
+#         de2 = e2.T * deriv(z1)
 
-#     dWo = np.dot( z1.T , de1 )
-#     dWx = np.dot( x.T, de2)
-#     Wo = Wo + dWo*lr
-#     Wx = Wx + dWx*lr
-    
-#     es.append(np.sum(np.abs(e1)))
+#         dWo = np.outer( z1 , de1 )
+#         dWx = np.outer( x[i], de2)
+#         Wo = Wo + dWo*lr
+#         Wx = Wx + dWx*lr
+        
+#         es.append(abs(e1))
 
 
 # plt.figure(1)
 # plt.plot(es)
 
-# z0 = x @ Wx
-# z0 = np.sin(z0)
-# z1 = z0 @ Wo
-# z1 = np.sin(z1)
+# for i in range(y.shape[0]):
+#     z0 = x[i] @ Wx
+#     z0 = act(z0)
+#     z1 = z0 @ Wo
+#     z1 = act(z1)
+    
+#     print(y[i], z1)
+    
 
-# for i in range(len(y)):
-#     print(y[i], z1[i])
